8fd13d2ebed14283f7f58dc2f9514f87047bbdca
Gustavo Kaash <gtf23@hotmail.com>
2020-03-02 20:43:55 -0300

    Update contact_page.dart

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -15,6 +15,7 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
 
+  final _nameFocus = FocusNode();
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
@@ -44,7 +45,13 @@ class _ContactPageState extends State<ContactPage> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          if(_editContact.name != null && _editContact.name.isNotEmpty){
+            Navigator.pop(context, _editContact);
+          } else {
+            FocusScope.of(context).requestFocus(_nameFocus);
+          }
+        },
           child: Icon(Icons.save),
         backgroundColor: Colors.red,
       ),
@@ -68,6 +75,7 @@ class _ContactPageState extends State<ContactPage> {
             ),
             TextField(
               controller: _nameController,
+              focusNode: _nameFocus,
               decoration: InputDecoration(labelText: "Nome"),
               onChanged: (text){
                 _userEdited = true;

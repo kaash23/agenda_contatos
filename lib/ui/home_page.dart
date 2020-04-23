5cc93c4d72055da153102a51e2820ffa1c85890b
Gustavo Kaash <gtf23@hotmail.com>
2020-04-23 17:08:47 -0300

    Update home_page.dart

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,10 +1,14 @@
 import 'dart:io';
+import 'contact_page.dart';
 
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import 'contact_page.dart';
+enum OrderOptions {orderaz, orderza}
+
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -31,6 +35,19 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contatos"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+                const PopupMenuItem<OrderOptions> (
+                    child: Text("Ordenar de A-Z"),
+                    value: OrderOptions.orderaz),
+                const PopupMenuItem<OrderOptions>(
+                    child: Text("Ordenar de Z-A"),
+                    value: OrderOptions.orderza),
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -105,7 +122,8 @@ class _HomePageState extends State<HomePage> {
               return Container(
                 padding: EdgeInsets.all(10.0),
                 child: Column(
-                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment:CrossAxisAlignment.center ,
+                  mainAxisSize: MainAxisSize.min ,//MainAxisSize.min,
                   children: <Widget>[
                     Padding(
                         padding: EdgeInsets.all(10.0),
@@ -113,12 +131,15 @@ class _HomePageState extends State<HomePage> {
                           child: Text("Ligar",
                             style: TextStyle(color: Colors.red, fontSize: 20.0),
                           ),
-                          onPressed: (){},
+                          onPressed: (){
+                            launch("tel:${contacts[index].phone}");
+                            Navigator.pop(context);
+                          },
                         )
                     ),Padding(
                         padding: EdgeInsets.all(10.0),
                         child: FlatButton(
-                          child: Text("Editarr",
+                          child: Text("Editar",
                             style: TextStyle(color: Colors.red, fontSize: 20.0),
                           ),
                           onPressed: (){
@@ -171,6 +192,25 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         contacts = list;
       });
+    });
+  }
+
+  void _orderList (OrderOptions results){
+    switch(results){
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+         return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {
+
     });
   }
 
